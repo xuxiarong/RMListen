0af38029cb2b509d89cff305bbabca89b4c9c038
xiyouji.xiaoyaoyou.lm81@gmail.com <xiyouji.xiaoyaoyou.lm81@gmail.com>
2020-08-03 11:06:38 +0800

    实现基类测试

--- app/src/main/java/com/rm/listen/MainActivity.kt
+++ app/src/main/java/com/rm/listen/MainActivity.kt
@@ -1,23 +1,15 @@
 package com.rm.listen
 
-import android.os.Bundle
 import com.lmm.basic.baselib.BaseActivity
 
 class MainActivity : BaseActivity() {
     override fun attachLayoutRes(): Int {
-        TODO("Not yet implemented")
+        return R.layout.activity_main
     }
 
     override fun initView() {
-        TODO("Not yet implemented")
     }
 
     override fun start() {
-        TODO("Not yet implemented")
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
