5ebe9de58c7bd530175615dc1d7b630c0df39ee4
xiyouji.xiaoyaoyou.lm81@gmail.com <xiyouji.xiaoyaoyou.lm81@gmail.com>
2020-08-03 10:35:09 +0800

    首次提交

---
+++ app/src/main/java/com/rm/listen/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.rm.listen
+
+import android.os.Bundle
+import com.lmm.basic.baselib.BaseActivity
+
+class MainActivity : BaseActivity() {
+    override fun attachLayoutRes(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun start() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
