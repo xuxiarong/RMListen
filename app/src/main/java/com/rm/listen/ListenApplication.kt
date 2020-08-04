9a7e954439b7c2031cdb006390e0ea490b7cf4a2
Suolongll <suolong520ll@gmail.com>
2020-08-04 15:03:37 +0800

    feat 添加分包dex的依赖库

---
+++ app/src/main/java/com/rm/listen/ListenApplication.kt
@@ -0,0 +1,22 @@
+package com.rm.listen
+
+import android.content.Context
+import androidx.multidex.MultiDex
+import com.lmm.basic.baselib.BaseApplication
+
+/**
+ * desc   :
+ * date   : 2020/08/04
+ * version: 1.0
+ */
+class ListenApplication : BaseApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this);
+    }
+}
