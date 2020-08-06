9fc79f51c5db41ef5370ce6333e535bd7e8110e6
zean <xiyouji.xiaoyaoyou.lm81@gmail.com>
2020-08-06 12:01:44 +0800

    Repository 统一解析处理

--- app/src/main/java/com/rm/listen/repository/LoginRepository.kt
+++ app/src/main/java/com/rm/listen/repository/LoginRepository.kt
@@ -1,11 +1,9 @@
 package com.rm.listen.repository
 
 import com.google.gson.Gson
-import com.lm.common.net.api.BaseApplication
 import com.lm.common.net.api.BaseRepository
 import com.lm.common.net.api.BaseResult
 import com.lm.common.util.Preference
-import com.rm.listen.R
 import com.rm.listen.api.ListenApiService
 import com.rm.listen.bean.User
 
@@ -21,28 +19,16 @@ class LoginRepository(val service: ListenApiService) : BaseRepository() {
 
 
     suspend fun login(userName: String, passWord: String): BaseResult<User> {
-        return safeApiCall(call = { requestLogin(userName, passWord) },
-            errorMessage = BaseApplication.CONTEXT.getString(R.string.app_name))
-    }
-
-    // TODO Move into DataSource Layer ?
-    private suspend fun requestLogin(userName: String, passWord: String): BaseResult<User> {
-        val response = service.login(userName, passWord)
-
-        return executeResponse(response, {
-            val user = response.data
+        return apiCall { service.login(userName, passWord) }.apply {
             isLogin = true
-            userJson = Gson().toJson(user)
-        })
+            userJson = Gson().toJson(this)
+        }
     }
 
     suspend fun register(userName: String, passWord: String): BaseResult<User> {
-        return safeApiCall(call = { requestRegister(userName, passWord) }, errorMessage = "注册失败")
+        return apiCall { service.register(userName, passWord, passWord) }.apply {
+            isLogin = true
+            userJson = Gson().toJson(this)
+        }
     }
-
-    private suspend fun requestRegister(userName: String, passWord: String): BaseResult<User> {
-        val response = service.register(userName, passWord, passWord)
-        return executeResponse(response, { requestLogin(userName, passWord) })
-    }
-
 }
--- common/src/main/java/com/lm/common/net/api/BaseRepository.kt
+++ common/src/main/java/com/lm/common/net/api/BaseRepository.kt
@@ -2,9 +2,7 @@ package com.lm.common.net.api
 
 import com.lm.common.net.bean.BaseResponse
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.withContext
 import java.io.IOException
 
 /**
@@ -13,40 +11,38 @@ import java.io.IOException
  * version: 1.0
  */
 open class BaseRepository {
+//    suspend fun <T : Any> apiCall(
+//        call: suspend () -> BaseResult<T>,
+//        errorMessage: String
+//    ): BaseResult<T> {
+//        return try {
+//            call()
+//        } catch (e: Exception) {
+//            // An exception was thrown when calling the API so we're converting this to an IOException
+//            BaseResult.Error(IOException(errorMessage, e))
+//        }
+//    }
 
-    suspend fun <T : Any> apiCall(call: suspend () -> BaseResponse<T>): BaseResponse<T> {
-        return withContext(Dispatchers.IO) { call.invoke() }
-    }
-
-    suspend fun <T : Any> safeApiCall(
-        call: suspend () -> BaseResult<T>,
-        errorMessage: String
-    ): BaseResult<T> {
+    suspend fun <T : Any> apiCall(call: suspend () -> BaseResponse<T>): BaseResult<T> {
         return try {
-            call()
+            executeResponse(call())
         } catch (e: Exception) {
-            // An exception was thrown when calling the API so we're converting this to an IOException
-            BaseResult.Error(IOException(errorMessage, e))
+            BaseResult.Error(e)
         }
     }
 
-    suspend fun <T : Any> executeResponse(
+    private suspend fun <T : Any> executeResponse(
         response: BaseResponse<T>, successBlock: (suspend CoroutineScope.() -> Unit)? = null,
         errorBlock: (suspend CoroutineScope.() -> Unit)? = null
     ): BaseResult<T> {
-        return try {
-            coroutineScope {
-                if (response.errorCode == -1) {
-                    errorBlock?.let { it() }
-                    BaseResult.Error(IOException(response.errorMsg))
-                } else {
-                    successBlock?.let { it() }
-                    BaseResult.Success(response.data)
-                }
+        return coroutineScope {
+            if (response.code == -1) {
+                errorBlock?.let { it() }
+                BaseResult.Error(IOException(response.msg))
+            } else {
+                successBlock?.let { it() }
+                BaseResult.Success(response.data)
             }
-        } catch (e: Exception) {
-            // An exception was thrown when calling the API so we're converting this to an IOException
-            BaseResult.Error(e)
         }
     }
 
