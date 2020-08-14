5b57afa8b2b4dc2abf8432e22c26f28efce19adb
zean <xiyouji.xiaoyaoyou.lm81@gmail.com>
2020-08-14 12:30:27 +0800

    module-login 独立app配置

---
+++ module-login/src/main/java/com/rm/module_login/app/LoginApplication.kt
@@ -0,0 +1,11 @@
+package com.rm.module_login.app
+
+import com.rm.baselisten.BaseApplication
+
+/**
+ * desc   :
+ * date   : 2020/08/13
+ * version: 1.0
+ */
+class LoginApplication : BaseApplication() {
+}
