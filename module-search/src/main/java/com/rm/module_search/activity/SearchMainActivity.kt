fd0577fc52b045d9cc3e67e5e9d7b7944efc276b
zean <xiyouji.xiaoyaoyou.lm81@gmail.com>
2020-08-14 10:55:08 +0800

    Search 配置可单独运行

---
+++ module-search/src/main/java/com/rm/module_search/activity/SearchMainActivity.kt
@@ -0,0 +1,19 @@
+package com.rm.module_search.activity
+
+import com.rm.baselisten.activity.BaseActivity
+import com.rm.module_search.R
+
+/**
+ * desc   :
+ * date   : 2020/08/13
+ * version: 1.0
+ */
+class SearchMainActivity : BaseActivity() {
+    override fun getLayoutResId(): Int = R.layout.search_activity_main
+
+    override fun initView() {
+    }
+
+    override fun initData() {
+    }
+}
