9bd24c3b9e8c06ff11f74991f3d24407e8cfd67a
zean <xiyouji.xiaoyaoyou.lm81@gmail.com>
2020-08-22 14:59:58 +0800

    fix ImageView 扩展函数

--- base-listen-lib/src/main/java/com/rm/baselisten/binding/BindingImage.kt
+++ base-listen-lib/src/main/java/com/rm/baselisten/binding/BindingImage.kt
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint
 import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-import com.bumptech.glide.Glide
+import com.rm.baselisten.thridlib.glide.loadImage
 
 /**
  * desc   :
@@ -25,13 +25,16 @@ fun ImageView.bindSrc(resourceId: Int?) {
 @SuppressLint("CheckResult")
 @BindingAdapter("bindUrl")
 fun ImageView.bindUrl(url: String) {
-    Glide.with(context).load(url)
-        .into(this)
+    loadImage(this,url)
 }
 
-@SuppressLint("CheckResult")
-@BindingAdapter("bindRoundUrl")
-fun ImageView.bindRoundUrl(url: String) {
-    Glide.with(context).load(url)
-        .into(this)
-}
+//@BindingAdapter("bindRoundUrl")
+//fun ImageView.bindRoundUrl(corner:Float,url: String) {
+//    loadRoundCornersImage(corner,this,url)
+//}
+//
+//@SuppressLint("CheckResult")
+//@BindingAdapter("bindCircleUrl")
+//fun ImageView.bindCircleUrl(url: String) {
+//    loadCircleImage(this,url)
+//}
