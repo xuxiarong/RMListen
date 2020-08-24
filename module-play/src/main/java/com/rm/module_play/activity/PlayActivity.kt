54b8a4653b118db48ba8a17382fd124d41bc6774
jingle <xiyouji.xiaoyaoyou.lm225@gmail.com>
2020-08-24 15:58:15 +0800

    viewmodel错误修正

--- module-play/src/main/java/com/rm/module_play/activity/PlayActivity.kt
+++ module-play/src/main/java/com/rm/module_play/activity/PlayActivity.kt
@@ -1,12 +1,14 @@
 package com.rm.module_play.activity
+
 import com.rm.baselisten.activity.BaseNetActivity
 import com.rm.baselisten.util.ToastUtil
 import com.rm.module_play.R
 import com.rm.module_play.viewmodel.PlayViewModel
 
-class PlayActivity : BaseNetActivity<com.rm.module_play.databinding.ActivityPlayBinding, PlayViewModel>() {
+class PlayActivity :
+    BaseNetActivity<com.rm.module_play.databinding.ActivityPlayBinding, PlayViewModel>() {
+
     override fun getLayoutId(): Int = R.layout.activity_play
-    override fun initViewModel(): PlayViewModel = mViewModel
     override fun initView() {
         dataBind.viewModel = mViewModel
     }
@@ -15,6 +17,6 @@ class PlayActivity : BaseNetActivity<com.rm.module_play.databinding.ActivityPlay
     }
 
     override fun initData() {
-        ToastUtil.show(this,mViewModel.msg)
+        ToastUtil.show(this, mViewModel.msg)
     }
 }
