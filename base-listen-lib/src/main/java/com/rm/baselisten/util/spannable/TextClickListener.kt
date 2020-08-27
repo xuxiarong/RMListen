94328d3e8982d721febc52a5251abd280c18c9d8
zean <xiyouji.xiaoyaoyou.lm81@gmail.com>
2020-08-27 10:56:34 +0800

    add 富文本帮助类    add 验证码登陆界面

---
+++ base-listen-lib/src/main/java/com/rm/baselisten/util/spannable/TextClickListener.kt
@@ -0,0 +1,10 @@
+package com.rm.baselisten.util.spannable
+
+/**
+ * desc   :
+ * date   : 2020/08/26
+ * version: 1.0
+ */
+interface TextClickListener {
+    fun onTextClick(clickContent: String)
+}
--- module-login/src/main/java/com/rm/module_login/viewmodel/LoginByVerifyViewModel.kt
+++ module-login/src/main/java/com/rm/module_login/viewmodel/LoginByVerifyViewModel.kt
@@ -1,7 +1,7 @@
 package com.rm.module_login.viewmodel
 
+import android.text.TextUtils
 import androidx.databinding.ObservableField
-import androidx.lifecycle.MutableLiveData
 import com.rm.baselisten.util.DLog
 import com.rm.baselisten.viewmodel.BaseNetViewModel
 import com.rm.module_login.repository.LoginRepository
@@ -12,16 +12,36 @@ import com.rm.module_login.repository.LoginRepository
  * version: 1.0
  */
 class LoginByVerifyViewModel(private val repository: LoginRepository) : BaseNetViewModel() {
-    // 输入框取消按钮是否显示标识
-    var isCancelBtnVisible = MutableLiveData<Boolean>(false)
-    var phone = ObservableField<String>("")
-
+    // 标识是否输入了文本
+    var isInputText = ObservableField<Boolean>()
+    //  输入的电话号码值
+    var phone = ObservableField<String>()
     // 国家代码具体值
-    var countryCode = MutableLiveData<String>()
+    var countryCode = ObservableField<String>()
+    // 用户协议和隐私协议是否选择
+    var isCheck = ObservableField<Boolean>(false)
 
-    val phoneNumber : (String ) -> Unit = {afterInputChange()}
+    // 监听绑定输入框内容变化
+    val checkInput: (String) -> Unit = { afterInputChange() }
 
-    fun afterInputChange() {
-        DLog.d("zean","phone = ${phone.get()}")
+    private fun afterInputChange() {
+        // 设置是否输入了文本
+        isInputText.set(!TextUtils.isEmpty(phone.get()))
+    }
+
+    /**
+     * 获取验证码
+     */
+    fun getCode(){
+        // TODO 网络通过手机获取验证码
+        if(!isCheck.get()!!){
+            // 未选中check box
+        }
+        DLog.i("llj","获取验证码！！！isCheck---->>${isCheck.get()}")
+    }
+
+    // 置空输入框的值
+    fun clearInput(){
+        phone.set("")
     }
 }
