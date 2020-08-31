436e5d96f0e4439215cb393f2e5b8ee73239d709
zean <xiyouji.xiaoyaoyou.lm81@gmail.com>
2020-08-31 17:03:55 +0800

    fix 提出公共的密码输入框 mvvm

--- module-login/src/main/java/com/rm/module_login/viewmodel/LoginByPasswordViewModel.kt
+++ module-login/src/main/java/com/rm/module_login/viewmodel/LoginByPasswordViewModel.kt
@@ -1,7 +1,6 @@
 package com.rm.module_login.viewmodel
 
 import android.app.Activity
-import android.text.TextUtils
 import androidx.databinding.ObservableField
 import com.rm.baselisten.util.ToastUtil
 import com.rm.baselisten.viewmodel.BaseVMViewModel
@@ -9,6 +8,7 @@ import com.rm.module_login.R
 import com.rm.module_login.activity.ForgetPasswordActivity
 import com.rm.module_login.activity.LoginByVerifyCodeActivity
 import com.rm.module_login.repository.LoginRepository
+import com.rm.module_login.viewmodel.view.PasswordInputViewModel
 import com.rm.module_login.viewmodel.view.PhoneInputViewModel
 
 /**
@@ -19,37 +19,18 @@ import com.rm.module_login.viewmodel.view.PhoneInputViewModel
 class LoginByPasswordViewModel(private val repository: LoginRepository) : BaseVMViewModel() {
 
     // 手机输入框的ViewModel
-    var phoneInputViewModel = PhoneInputViewModel()
+    val phoneInputViewModel = PhoneInputViewModel()
+
+    // 密码输入框的ViewModel
+    val passwordInputViewModel = PasswordInputViewModel()
 
     var context: Activity? = null
-
-    //  输入的密码值
-    var password = ObservableField<String>()
 
     // 用户协议和隐私协议是否选择
     var isCheck = ObservableField<Boolean>(false)
 
     // 错误提示信息
     var errorTips = ObservableField<String>("")
-
-    // 密码输入是否ok
-    var isPasswordInputOk = ObservableField<Boolean>(false)
-
-    // 密码是否显示按钮的资源id
-    var eyesResId = ObservableField<Int>(R.drawable.login_ic_close_eyes)
-
-    // 是否明文显示密码文本
-    var isShowPasswordText = ObservableField<Boolean>(false)
-
-    // 监听绑定输入框内容变化
-    val checkInput: (String) -> Unit = { inputChange() }
-
-    private fun inputChange() {
-        // 设置是否输入了文本
-//        isPhoneInputOk.set(!TextUtils.isEmpty(phoneInputViewModel.phone.get()))
-        isPasswordInputOk.set(!TextUtils.isEmpty(password.get()))
-//        isInputText.set(isPhoneInputOk.get()!! && isPasswordInputOk.get()!!)
-    }
 
     /**
      * 登陆
@@ -62,21 +43,6 @@ class LoginByPasswordViewModel(private val repository: LoginRepository) : BaseVM
             return
         }
 
-    }
-
-    /**
-     * 密码显示按钮点击事件
-     */
-    fun eyesClick() {
-        if (eyesResId.get() == R.drawable.login_ic_close_eyes) {
-            // 显示密码
-            eyesResId.set(R.drawable.login_ic_open_eyes)
-            isShowPasswordText.set(true)
-        } else {
-            // 隐藏密码
-            eyesResId.set(R.drawable.login_ic_close_eyes)
-            isShowPasswordText.set(false)
-        }
     }
 
     /**
