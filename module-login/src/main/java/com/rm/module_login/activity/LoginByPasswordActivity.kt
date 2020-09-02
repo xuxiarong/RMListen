ec8a93778f2f72c1a777b5b1a476a0ac674e26e8
zean <xiyouji.xiaoyaoyou.lm81@gmail.com>
2020-09-02 19:37:14 +0800

    add 密码登陆界面接口对接

--- module-login/src/main/java/com/rm/module_login/activity/LoginByPasswordActivity.kt
+++ module-login/src/main/java/com/rm/module_login/activity/LoginByPasswordActivity.kt
@@ -7,6 +7,7 @@ import com.rm.baselisten.util.ToastUtil
 import com.rm.baselisten.util.spannable.ChangeItem
 import com.rm.baselisten.util.spannable.SpannableHelper
 import com.rm.baselisten.util.spannable.TextClickListener
+import com.rm.baselisten.utilExt.String
 import com.rm.module_login.BR
 import com.rm.module_login.R
 import com.rm.module_login.databinding.LoginActivityLoginByPassowrdBinding
@@ -35,21 +36,57 @@ class LoginByPasswordActivity :
 
     override fun initView() {
         super.initView()
-        mViewModel.context = this
+        mViewModel.callBackToActivity = { callBackFromViewModel(it) }
     }
 
     override fun initData() {
         // 设置checkbox选择协议相关文本
-        SpannableHelper.with(login_by_password_tips,resources.getString(R.string.login_login_tips_all))
-            .addChangeItem(ChangeItem(resources.getString(R.string.login_login_tips_user),ChangeItem.Type.COLOR,resources.getColor(R.color.login_high_color),object :TextClickListener{
-                override fun onTextClick(clickContent: String) {
-                    ToastUtil.show(this@LoginByPasswordActivity,"用户协议")
-                }
-            }))
-            .addChangeItem(ChangeItem(resources.getString(R.string.login_login_tips_privacy),ChangeItem.Type.COLOR,resources.getColor(R.color.login_high_color),object :TextClickListener{
-                override fun onTextClick(clickContent: String) {
-                    ToastUtil.show(this@LoginByPasswordActivity,"隐私保护协议")
-                }
-            })).build()
+        SpannableHelper.with(
+            login_by_password_tips,
+            resources.getString(R.string.login_login_tips_all)
+        )
+            .addChangeItem(
+                ChangeItem(
+                    resources.getString(R.string.login_login_tips_user),
+                    ChangeItem.Type.COLOR,
+                    resources.getColor(R.color.login_high_color),
+                    object : TextClickListener {
+                        override fun onTextClick(clickContent: String) {
+                            ToastUtil.show(this@LoginByPasswordActivity, "用户协议")
+                        }
+                    })
+            )
+            .addChangeItem(
+                ChangeItem(
+                    resources.getString(R.string.login_login_tips_privacy),
+                    ChangeItem.Type.COLOR,
+                    resources.getColor(R.color.login_high_color),
+                    object : TextClickListener {
+                        override fun onTextClick(clickContent: String) {
+                            ToastUtil.show(this@LoginByPasswordActivity, "隐私保护协议")
+                        }
+                    })
+            ).build()
+    }
+
+    private fun callBackFromViewModel(flag: Int, params: String = "") {
+        when (flag) {
+            0 -> ToastUtil.show(this, String(R.string.login_agree_deal_tips)) // 未同意协议
+
+            1 -> ForgetPasswordActivity.startActivity(this) // 忘记密码
+
+            2 -> {
+                // 验证码登陆
+                LoginByVerifyCodeActivity.startActivity(this)
+                finish()
+            }
+
+            3 -> {
+                // 登陆成功
+                ToastUtil.show(this, String(R.string.login_success))
+                finish()
+            }
+
+        }
     }
 }
--- module-login/src/main/java/com/rm/module_login/repository/LoginRepository.kt
+++ module-login/src/main/java/com/rm/module_login/repository/LoginRepository.kt
@@ -4,6 +4,7 @@ import com.rm.baselisten.net.api.BaseRepository
 import com.rm.baselisten.net.api.BaseResult
 import com.rm.baselisten.util.getStringMMKV
 import com.rm.business_lib.REFRESH_TOKEN
+import com.rm.business_lib.utils.aes.AESUtil
 import com.rm.module_login.api.LoginApiService
 import com.rm.module_login.bean.LoginInfo
 
@@ -49,6 +50,18 @@ class LoginRepository(private val apiService: LoginApiService) : BaseRepository(
      */
     suspend fun loginByVerifyCode(phone: String, code: String): BaseResult<LoginInfo> {
         return apiCall { apiService.loginByVerifyCode(phone, code) }
+    }
+
+    /**
+     * 密码登陆
+     * @param phone String
+     * @param password String
+     * @return BaseResult<LoginInfo>
+     */
+    suspend fun loginByPassword(phone: String, password: String): BaseResult<LoginInfo> {
+        // 加密密码
+        val encryptPassword = AESUtil.encryptString2Base64(password,"a48435c7e0930cd2c5f2a0c8a3ec29fe","140fa03a972cdb3c")
+        return apiCall { apiService.loginByPassword(phone, encryptPassword) }
     }
 
     /**
