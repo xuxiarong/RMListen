73e42e829461d9db799f9bc8c65a31882d08f314
Suolongll <suolong520ll@gmail.com>
2020-09-02 15:48:16 +0800

    fix 开发听单页

---
+++ module-listen/src/main/java/com/rm/module_listen/viewmodel/ListenMyListenViewModel.kt
@@ -0,0 +1,12 @@
+package com.rm.module_listen.viewmodel
+
+import com.rm.baselisten.viewmodel.BaseVMViewModel
+
+/**
+ * desc   :
+ * date   : 2020/09/02
+ * version: 1.0
+ */
+class ListenMyListenViewModel : BaseVMViewModel() {
+
+}
