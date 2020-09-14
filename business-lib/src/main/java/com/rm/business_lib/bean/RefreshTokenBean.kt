fbe51c7051e0efdec2de979147ad24e6a8d87b43
zean <xiyouji.xiaoyaoyou.lm81@gmail.com>
2020-09-14 17:22:45 +0800

    拦截返回参数监听处理(有问题，还为解决，暂时提交)

---
+++ business-lib/src/main/java/com/rm/business_lib/bean/RefreshTokenBean.kt
@@ -0,0 +1,8 @@
+package com.rm.business_lib.bean
+
+/**
+ * desc   : 刷新token返回bean
+ * date   : 2020/09/14
+ * version: 1.0
+ */
+data class RefreshTokenBean(val access: String, val refresh: String)
--- business-lib/src/main/java/com/rm/business_lib/net/CustomInterceptor.kt
+++ business-lib/src/main/java/com/rm/business_lib/net/CustomInterceptor.kt
@@ -1,14 +1,25 @@
 package com.rm.business_lib.net
 
 import android.os.Build
+import com.google.gson.Gson
 import com.rm.baselisten.BuildConfig
+import com.rm.baselisten.net.bean.BaseResponse
+import com.rm.baselisten.util.DLog
 import com.rm.baselisten.util.encodeMD5
 import com.rm.baselisten.util.getStringMMKV
 import com.rm.business_lib.ACCESS_TOKEN
+import com.rm.business_lib.REFRESH_TOKEN
+import com.rm.business_lib.bean.RefreshTokenBean
 import com.rm.business_lib.utils.DeviceUtils
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
+import okhttp3.ResponseBody
+import retrofit2.http.POST
+import retrofit2.http.Query
 
 /**
  * desc   : 业务网络拦截器
@@ -20,11 +31,25 @@ class CustomInterceptor : Interceptor {
     // 服务器固定的 Android端app_key
     private val APP_KEY = "5d25eb5bf85ef867e78172d0d0df5ef0"
 
+    companion object {
+        var flag = 0
+    }
+
+    private val apiService by lazy {
+        BusinessRetrofitClient().getService(
+            RefreshApiService::class.java
+        )
+    }
+
     override fun intercept(chain: Interceptor.Chain): Response {
+        DLog.i("llj", "当前线程----->>>${Thread.currentThread().name}")
         val original = chain.request()
         val request: Request = getRequestHeaderBuilder(original.newBuilder()).build()
         val originalResponse = arrayOf(chain.proceed(request))
         return originalResponse[0]
+//        // 返回参数拦截处理
+//        responseIntercept(originalResponse, chain)
+//        return originalResponse[0]
     }
 
 
@@ -59,4 +84,73 @@ class CustomInterceptor : Interceptor {
         val nonceItem: (Int) -> Char = { nonceScope[(nonceScope.length * Math.random()).toInt()] }
         return Array(16, nonceItem).joinToString("")
     }
+
+
+    /**
+     * 拦截返回数据，判断是否需要刷新token
+     * @param response Response
+     * @return Response
+     */
+    private fun responseIntercept(originalResponse: Array<Response>, chain: Interceptor.Chain) {
+
+        //保存body的相关信息，用于后续构造新的Response
+        val responseBody = originalResponse[0].body
+        val contentType = responseBody?.contentType()
+        val responseStr = responseBody?.string()
+        responseBody?.close()
+        val baseResponse = Gson().fromJson(responseStr, BaseResponse::class.java)
+
+//        if (baseResponse.code == 0) {
+        if (flag == 0) {
+            flag++
+            // token 过期，需要刷新token
+            GlobalScope.launch {
+                val refreshResponse = GlobalScope.async {
+                    apiService.refreshToken(REFRESH_TOKEN.getStringMMKV(""))
+                }
+                if (refreshResponse.await().code != 0) {
+                    // 刷新成功,再次去请求之前的接口
+                    DLog.i("llj", "刷新成功,再次去请求之前的接口")
+                    val request: Request.Builder =
+                        getRequestHeaderBuilder(chain.request().newBuilder())
+                    try {
+                        originalResponse[0] = chain.proceed(request.build())
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+
+                } else {
+                    // 刷新失败，退出登陆
+                    DLog.e("llj", "刷新token失败！！")
+                    originalResponse[0] = originalResponse[0].newBuilder().code(200).body(
+                        responseStr?.let {
+                            ResponseBody.create(
+                                contentType,
+                                it
+                            )
+                        }).build()
+                }
+            }
+
+        } else {
+            DLog.i("llj", "返回码------>>>${baseResponse.code}")
+            originalResponse[0] = originalResponse[0].newBuilder().code(200).body(responseStr?.let {
+                ResponseBody.create(
+                    contentType,
+                    it
+                )
+            }).build()
+        }
+    }
+}
+
+interface RefreshApiService {
+    /**
+     * 刷新token(令牌)
+     * @param refreshToken String
+     */
+    @POST("auth/refresh")
+    suspend fun refreshToken(
+        @Query("refresh_token") refreshToken: String
+    ): BaseResponse<RefreshTokenBean>
 }
