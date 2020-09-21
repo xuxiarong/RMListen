16e69873336af54c549d32c66e586067c6e69361
zean <xiyouji.xiaoyaoyou.lm81@gmail.com>
2020-09-21 12:22:22 +0800

    fix SmartRefreshLayout 首页不显示上拉加载更多

--- business-lib/src/main/java/com/rm/business_lib/BusinessApplication.kt
+++ business-lib/src/main/java/com/rm/business_lib/BusinessApplication.kt
@@ -38,7 +38,7 @@ open class BusinessApplication : BaseApplication() {
             layout.setPrimaryColorsId(R.color.base_activity_bg_color) //全局设置主题颜色
             layout.setEnableLoadMoreWhenContentNotFull(false)
             layout.setEnableFooterFollowWhenNoMoreData(true)
-            layout.setEnableLoadMore(true)
+//            layout.setEnableLoadMore(true)
             ClassicsFooter(context) //.setTimeFormat(new DynamicTimeFormat("更新于 %s"));//指定为经典Header，默认是 贝塞尔雷达Header
         }
     }
