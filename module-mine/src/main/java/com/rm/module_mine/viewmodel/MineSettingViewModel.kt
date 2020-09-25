21813c68d07c3a15adc54e7fba4d65dd024decfa
zean <xiyouji.xiaoyaoyou.lm81@gmail.com>
2020-09-25 16:57:33 +0800

    add 退出登陆添加弹出框 个人资料点击判断是否登陆操作

--- module-mine/src/main/java/com/rm/module_mine/viewmodel/MineSettingViewModel.kt
+++ module-mine/src/main/java/com/rm/module_mine/viewmodel/MineSettingViewModel.kt
@@ -1,9 +1,16 @@
 package com.rm.module_mine.viewmodel
 
+import android.content.Context
+import androidx.fragment.app.FragmentActivity
+import com.rm.baselisten.utilExt.String
 import com.rm.baselisten.viewmodel.BaseVMViewModel
+import com.rm.business_lib.base.dialog.TipsFragmentDialog
 import com.rm.business_lib.helpter.loginOut
 import com.rm.business_lib.isLogin
 import com.rm.business_lib.loginUser
+import com.rm.component_comm.login.LoginService
+import com.rm.component_comm.router.RouterHelper
+import com.rm.module_mine.R
 import com.rm.module_mine.activity.MineAccountSecuritySettingActivity
 import com.rm.module_mine.activity.MineDownloadSettingActivity
 import com.rm.module_mine.activity.MinePersonalInfoActivity
@@ -24,14 +31,32 @@ class MineSettingViewModel : BaseVMViewModel() {
     /**
      * 登出
      */
-    fun loginOutClick() {
-        loginOut()
+    fun loginOutClick(context: Context) {
+        TipsFragmentDialog().apply {
+            contentText = context.String(R.string.mine_login_out_tips)
+            leftBtnText = context.String(R.string.mine_login_out_sure)
+            rightBtnText = context.String(R.string.mine_login_out_cancel)
+            rightBtnTextColor = R.color.business_color_ff5e5e
+            leftBtnClick = {
+                loginOut()
+                finish()
+            }
+            rightBtnClick = {
+                dismiss()
+            }
+        }.show(context as FragmentActivity)
+
     }
 
     /**
      * 个人资料
      */
-    fun clickSettingPersonal() {
+    fun clickSettingPersonal(context: Context) {
+        if (!isLogin.get()) {
+            RouterHelper.createRouter(LoginService::class.java)
+                .quicklyLogin(this, context as FragmentActivity)
+            return
+        }
         startActivity(MinePersonalInfoActivity::class.java)
     }
 
