9fc0f8f6e971d2e863d5094610ec9d6114fd6ec6
Suolongll <suolong520ll@gmail.com>
2020-09-28 18:06:20 +0800

    fix 修复订阅更新的UI和接口问题

--- module-listen/src/main/java/com/rm/module_listen/viewmodel/ListenSubsUpdateViewModel.kt
+++ module-listen/src/main/java/com/rm/module_listen/viewmodel/ListenSubsUpdateViewModel.kt
@@ -1,7 +1,8 @@
 package com.rm.module_listen.viewmodel
 
 import androidx.databinding.ObservableBoolean
-import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.chad.library.adapter.base.entity.MultiItemEntity
 import com.rm.baselisten.adapter.multi.CommonMultiVMAdapter
 import com.rm.baselisten.adapter.single.CommonBindVMAdapter
@@ -31,25 +32,33 @@ class ListenSubsUpdateViewModel : BaseVMViewModel() {
         ListenMyListenRepository(BusinessRetrofitClient().getService(ListenApiService::class.java))
     }
 
-    val subsAdapter = CommonMultiVMAdapter(this, mutableListOf(), BR.viewModel, BR.item)
+    val subsAudioAdapter = CommonMultiVMAdapter(this, mutableListOf(), BR.viewModel, BR.item)
 
-    val subsDateAdapter = CommonBindVMAdapter<ListenSubsDateModel>(this, mutableListOf(),R.layout.listen_item_subs_top_date,BR.viewModel,BR.item)
+    val subsDateAdapter = CommonBindVMAdapter<ListenSubsDateModel>(
+        this,
+        mutableListOf(),
+        R.layout.listen_item_subs_top_date,
+        BR.viewModel,
+        BR.item
+    )
+
+    private var currentDatePosition = 0
+    private var currentSubsDateFirstPosition = 0
+    private var isClickScroll = false
 
     // 下拉刷新和加载更多控件状态控制Model
     val refreshStatusModel = SmartRefreshLayoutStatusModel()
 
     var subsDateVisible = ObservableBoolean(true)
-    private var allSubsRvList = MutableLiveData<MutableList<MultiItemEntity>>()
-    private var allAudioList = MutableLiveData<MutableList<ListenAudio>>()
-    private var allDateList = MutableLiveData<MutableList<ListenSubsDateModel>>()
+//    private var allSubsRvList = MutableLiveData<MutableList<MultiItemEntity>>()
+//    private var allAudioList = MutableLiveData<MutableList<ListenAudio>>()
+//    private var allDateList = MutableLiveData<MutableList<ListenSubsDateModel>>()
 
     private var currentPage = 1
     val pageSize = 12
 
-    init {
-        allAudioList.value = ArrayList()
-        allSubsRvList.value = ArrayList()
-        allDateList.value = ArrayList()
+    fun init() {
+        initSubsRvScrollListen()
     }
 
     fun getSubsDataFromService() {
@@ -58,16 +67,13 @@ class ListenSubsUpdateViewModel : BaseVMViewModel() {
             repository.getListenSubsUpgradeList(currentPage, pageSize).checkResult(
                 onSuccess = {
                     showContentView()
+                    refreshStatusModel.setHasMore(it.list.size > 0)
                     refreshStatusModel.finishLoadMore(true)
                     currentPage++
-                    if(it.list.size<=pageSize){
-                        refreshStatusModel.setHasMore(false)
-                    }
                     dealData(it.list)
-
                 }, onError = {
                     showNetError()
-                    refreshStatusModel.finishLoadMore(true)
+                    refreshStatusModel.finishLoadMore(false)
                     DLog.d("suolong", "on error")
                 }
             )
@@ -76,10 +82,20 @@ class ListenSubsUpdateViewModel : BaseVMViewModel() {
 
 
     private fun dealData(chapterList: ArrayList<ListenAudioChapter>) {
+        sortChapter(chapterList)
         val checkedChapterList = checkLastAudioContainerChapter(checkChapterList = chapterList)
         getAudioList(checkedChapterList)
     }
 
+    private fun sortChapter(chapterList: ArrayList<ListenAudioChapter>) {
+        chapterList.sortWith(Comparator { o1, o2 ->
+            return@Comparator if (o1.upgrade_time < o2.upgrade_time) {
+                1
+            } else {
+                -1
+            }
+        })
+    }
 
 
     /**
@@ -87,7 +103,7 @@ class ListenSubsUpdateViewModel : BaseVMViewModel() {
      * @param chapterList List<ListenAudioChapter>
      * @return ArrayList<ListenAudio>
      */
-    private fun getAudioList(chapterList: List<ListenAudioChapter>){
+    private fun getAudioList(chapterList: List<ListenAudioChapter>) {
         val audioList = ArrayList<ListenAudio>()
         chapterList.forEach { chapter ->
             var hasChapter = false
@@ -121,18 +137,34 @@ class ListenSubsUpdateViewModel : BaseVMViewModel() {
             }
         })
 
-        audioList.forEach {
-            if(subsDateAdapter.data.size>0){
-                val lastDate = subsDateAdapter.data[subsDateAdapter.data.lastIndex]
-                if(lastDate.date != TimeUtils.getDateStr(it.info.upgrade_time)){
-                    subsAdapter.addData(ListenSubsDateModel(date = TimeUtils.getDateStr(it.info.upgrade_time)))
-                    subsDateAdapter.addData(ListenSubsDateModel(date = TimeUtils.getDateStr(it.info.upgrade_time),isTopRvItem = true))
+        audioList.forEach { audio ->
+            if (subsDateAdapter.data.size > 0) {
+                var hasAudioDate = false
+                subsDateAdapter.data.forEach { subsDate ->
+                    if (subsDate.date == TimeUtils.getDateStr(audio.info.upgrade_time)) {
+                        hasAudioDate = true
+                    }
                 }
-            }else{
-                subsDateAdapter.addData(ListenSubsDateModel(date = TimeUtils.getDateStr(it.info.upgrade_time),isSelected = true,isTopRvItem = true))
-                subsAdapter.addData(ListenSubsDateModel(date = TimeUtils.getDateStr(it.info.upgrade_time),isSelected = true))
+                if (!hasAudioDate) {
+                    subsAudioAdapter.addData(ListenSubsDateModel(date = TimeUtils.getDateStr(audio.info.upgrade_time)))
+                    subsDateAdapter.addData(
+                        ListenSubsDateModel(
+                            date = TimeUtils.getDateStr(audio.info.upgrade_time),
+                            isTopRvItem = true
+                        )
+                    )
+                }
+            } else {
+                subsDateAdapter.addData(
+                    ListenSubsDateModel(
+                        date = TimeUtils.getDateStr(audio.info.upgrade_time),
+                        isSelected = true,
+                        isTopRvItem = true
+                    )
+                )
+                subsAudioAdapter.addData(ListenSubsDateModel(date = TimeUtils.getDateStr(audio.info.upgrade_time)))
             }
-            subsAdapter.addData(it)
+            subsAudioAdapter.addData(audio)
         }
     }
 
@@ -142,20 +174,19 @@ class ListenSubsUpdateViewModel : BaseVMViewModel() {
      * @return ArrayList<ListenAudioChapter>
      */
     private fun checkLastAudioContainerChapter(checkChapterList: ArrayList<ListenAudioChapter>): ArrayList<ListenAudioChapter> {
-        val tempAllAudioList = allAudioList.value
+        val tempAllAudioList = subsAudioAdapter.data
         val iterator = checkChapterList.iterator()
         while (iterator.hasNext()) {
             val chapter = iterator.next()
             chapter.itemType = R.layout.listen_item_subs_list_chapter
-            if (tempAllAudioList != null && tempAllAudioList.size > 0) {
-                val lastAudio = tempAllAudioList.last()
+            if (tempAllAudioList.size > 0) {
+                val lastAudio = tempAllAudioList.last() as ListenAudio
                 if (lastAudio.info.audio_id == chapter.audio_id) {
                     lastAudio.chapter.add(chapter)
                     iterator.remove()
                 }
             }
         }
-        allAudioList.value = tempAllAudioList
         return checkChapterList
     }
 
@@ -176,9 +207,104 @@ class ListenSubsUpdateViewModel : BaseVMViewModel() {
         )
     }
 
-    fun onTopDateClick(model : ListenSubsDateModel){
+    /**
+     * 头部的日期Rv某个Item被点击了
+     * @param model ListenSubsDateModel
+     */
+    fun onTopDateClick(model: ListenSubsDateModel) {
+        val clickPosition = subsDateAdapter.data.indexOf(model)
+        if (clickPosition == currentDatePosition) {
+            return
+        }
+        isClickScroll = true
+        val lastModel = subsDateAdapter.data[currentDatePosition]
+        lastModel.isSelected = false
+        subsDateAdapter.notifyItemChanged(currentDatePosition)
         model.isSelected = true
-        subsDateAdapter.notifyItemChanged(subsAdapter.data.indexOf(model))
+        currentDatePosition = clickPosition
+        subsDateAdapter.notifyItemChanged(clickPosition)
+        subsDateAdapter.recyclerView.smoothScrollToPosition(clickPosition)
+        subsRvScrollToSelectDate(model)
+
+    }
+
+    /**
+     * 滑动到日期点击的那个Item
+     * @param model ListenSubsDateModel
+     */
+    private fun subsRvScrollToSelectDate(model: ListenSubsDateModel) {
+        try {
+            val indexOf = subsAudioAdapter.data.indexOf(
+                ListenSubsDateModel(date = model.date)
+            )
+            subsAudioAdapter.recyclerView.smoothScrollToPosition(indexOf)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    /**
+     * 初始化书籍的Rv的滑动事件
+     */
+    private fun initSubsRvScrollListen() {
+        subsAudioAdapter.recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if (isClickScroll) {
+                    return
+                }
+                super.onScrolled(recyclerView, dx, dy)
+                try {
+                    val linearLayoutManager: LinearLayoutManager =
+                        subsAudioAdapter.recyclerView.layoutManager as LinearLayoutManager
+                    val firstVisiblePosition = linearLayoutManager.findFirstVisibleItemPosition()
+                    if (firstVisiblePosition == currentSubsDateFirstPosition) {
+                        return
+                    }
+                    if (subsAudioAdapter.getItemViewType(firstVisiblePosition) == R.layout.listen_item_subs_list_audio_date) {
+                        if (firstVisiblePosition != currentSubsDateFirstPosition) {
+                            currentSubsDateFirstPosition = firstVisiblePosition
+                            val subsDateModel =
+                                subsAudioAdapter.data[firstVisiblePosition] as ListenSubsDateModel
+                            changeTopDateWithAudioScroll(subsDateModel)
+                        }
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    isClickScroll = false
+                }
+            }
+        })
+    }
+
+    /**
+     * 头部日期的Rv根据书籍列表的滑动效果更新显示效果
+     * @param model ListenSubsDateModel
+     */
+    fun changeTopDateWithAudioScroll(model: ListenSubsDateModel) {
+        try {
+            for (i in 0 until subsDateAdapter.data.size) {
+                if (subsDateAdapter.data[i].date == model.date) {
+                    subsDateAdapter.data[i].isSelected = true
+                    subsDateAdapter.notifyItemChanged(i)
+                    subsDateAdapter.recyclerView.smoothScrollToPosition(i)
+                    if (i != currentDatePosition) {
+                        subsDateAdapter.data[currentDatePosition].isSelected = false
+                        subsDateAdapter.notifyItemChanged(currentDatePosition)
+                        currentDatePosition = i
+                    }
+                    return
+                }
+            }
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
 }
