164377e2afb14e0681386bd11cebf2eb0550242e
Suolongll <suolong520ll@gmail.com>
2020-10-14 15:04:17 +0800

    fix 修复播放量的显示

---
+++ business-lib/src/main/java/com/rm/business_lib/binding/BindPlayCount.kt
@@ -0,0 +1,33 @@
+package com.rm.business_lib.binding
+
+import android.annotation.SuppressLint
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+
+/**
+ * desc   :
+ * date   : 2020/10/14
+ * version: 1.0
+ */
+@SuppressLint("SetTextI18n")
+@BindingAdapter("bindPlayCount")
+fun TextView.bindPlayCount(count :Int){
+    if (count<10000){
+        text = count.toString()
+    }else{
+        val wan = count/10000
+        val qian = count%10000/1000
+        val bai = count%1000/100
+        text = "$wan"+"."+"$qian"+"$bai"+"w"
+    }
+}
+@BindingAdapter("bindPlayCountString")
+fun TextView.bindPlayCountString(count :String?){
+    try{
+        if(count!=null){
+            bindPlayCount(count.toInt())
+        }
+    }catch (e : Exception){
+        e.printStackTrace()
+    }
+}
