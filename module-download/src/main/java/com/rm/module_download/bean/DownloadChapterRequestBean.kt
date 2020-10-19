8f7830a982d2016314928ecc5849e0fa310dcdb8
Suolongll <suolong520ll@gmail.com>
2020-10-19 11:30:21 +0800

    fix 修复下载页跳转的问题

--- module-download/src/main/java/com/rm/module_download/bean/DownloadChapterRequestBean.kt
+++ module-download/src/main/java/com/rm/module_download/bean/DownloadChapterRequestBean.kt
@@ -1,4 +1,3 @@
 package com.rm.module_download.bean
 
 data class DownloadChapterRequestBean(var audio_id: Long, var start_sequence: Int, var end_sequence: Int, var sequences: List<Int>, var type: Int)
-data class DownloadAudioRequestBean(var audio_id: Long, var page: Int = 1, var page_size: Int = 12, var chapter_id: Long = 0L, var sort: String = "asc")
