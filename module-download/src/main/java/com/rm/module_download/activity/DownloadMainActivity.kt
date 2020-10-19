854ce216bceeb7f00656de37029df045cf670858
Suolongll <suolong520ll@gmail.com>
2020-10-19 11:35:40 +0800

    fix 下载页没有返回的点击事件

--- module-download/src/main/java/com/rm/module_download/activity/DownloadMainActivity.kt
+++ module-download/src/main/java/com/rm/module_download/activity/DownloadMainActivity.kt
@@ -3,19 +3,13 @@ package com.rm.module_download.activity
 import android.content.Context
 import android.content.Intent
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
 import com.rm.baselisten.mvvm.BaseVMActivity
-import com.rm.business_lib.isLogin
-import com.rm.business_lib.loginUser
 import com.rm.module_download.BR
 import com.rm.module_download.R
 import com.rm.module_download.adapter.DownloadDetailPagerAdapter
-import com.rm.module_download.bean.DownloadChapterItemBean
 import com.rm.module_download.databinding.DownloadActivityDownloadMainBinding
 import com.rm.module_download.fragment.DownloadCompletedFragment
 import com.rm.module_download.fragment.DownloadInProgressFragment
-import com.rm.module_download.viewmodel.DownloadChapterSelectionViewModel
 import com.rm.module_download.viewmodel.DownloadMainViewModel
 import kotlinx.android.synthetic.main.download_activity_download_main.*
 
@@ -51,7 +45,6 @@ class DownloadMainActivity : BaseVMActivity<DownloadActivityDownloadMainBinding,
 
     override fun initView() {
         super.initView()
-//        mViewModel.baseTitleModel.value = BaseTitleModel().setNoTitle(true)
         download_detail_view_pager2.offscreenPageLimit = 2
         download_detail_view_pager2.adapter = mPagerAdapter
 
@@ -59,6 +52,8 @@ class DownloadMainActivity : BaseVMActivity<DownloadActivityDownloadMainBinding,
             addTab(getString(R.string.download_downloading))
             addTab(getString(R.string.download_downloaded))
         }.bindViewPager2(download_detail_view_pager2)
-
+        download_iv_back.setOnClickListener {
+            finish()
+        }
     }
 }
