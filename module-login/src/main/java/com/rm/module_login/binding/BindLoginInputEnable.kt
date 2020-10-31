453d2ab35db99f70abd7a43f171da84069a9f2e1
zean <xiyouji.xiaoyaoyou.lm81@gmail.com>
2020-10-31 14:52:29 +0800

    fix bug:验证密码规则改动，8-16位，可位字母+数字+符号任意其中两种组合

--- module-login/src/main/java/com/rm/module_login/binding/BindLoginInputEnable.kt
+++ module-login/src/main/java/com/rm/module_login/binding/BindLoginInputEnable.kt
@@ -26,10 +26,17 @@ fun View.bindLoginPhoneAndPasswordEnable(countryCode: String?, phone: String?, p
     } else if (phone == null && password != null) {
         // 只是验证密码是否ok
         // 密码验证规则正则表达式
-        val passwordRegex = Regex("^(?![0-9]+\$)(?![a-zA-Z]+\$)[0-9A-Za-z]{6,16}\$")
+        // 密码的正则表达式
+//    private val passwordRegex = Regex("^(?![0-9]+\$)(?![a-zA-Z]+\$)[0-9A-Za-z]{6,16}\$")
+        // 密码的正则表达式(至少包含字母/数字/特殊字符 其中两种类型组合)
+        val passwordRegex =
+            Regex("(?!^\\d+\$)(?!^[A-Za-z]+\$)(?!^[^A-Za-z0-9]+\$)(?!^.*[\\u4E00-\\u9FA5].*\$)^\\S{8,16}\$")
         passwordRegex.matches(password)
     } else {
-        val passwordRegex = Regex("^(?![0-9]+\$)(?![a-zA-Z]+\$)[0-9A-Za-z]{6,16}\$")
+//        val passwordRegex = Regex("^(?![0-9]+\$)(?![a-zA-Z]+\$)[0-9A-Za-z]{8,16}\$")
+        // 密码的正则表达式(至少包含字母/数字/特殊字符 其中两种类型组合)
+        val passwordRegex =
+            Regex("(?!^\\d+\$)(?!^[A-Za-z]+\$)(?!^[^A-Za-z0-9]+\$)(?!^.*[\\u4E00-\\u9FA5].*\$)^\\S{8,16}\$")
         if (phone != null && countryCode != null && TextUtils.equals(countryCode, "+86")) {
             // 如果是中国区的电话号码，则必须11位数
             phone.length == 11 && passwordRegex.matches(password!!)
