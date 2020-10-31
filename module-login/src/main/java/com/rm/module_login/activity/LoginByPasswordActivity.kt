d9651af6b47daac754bf3484cb1b87d331078d32
zean <xiyouji.xiaoyaoyou.lm81@gmail.com>
2020-10-31 17:15:25 +0800

    fix bug: 登陆界面添加顶部背景

--- module-login/src/main/java/com/rm/module_login/activity/LoginByPasswordActivity.kt
+++ module-login/src/main/java/com/rm/module_login/activity/LoginByPasswordActivity.kt
@@ -42,6 +42,7 @@ class LoginByPasswordActivity :
 
     override fun initView() {
         super.initView()
+        setTransparentStatusBar()
         login_include_phone_input_country_code.setOnClickListener {
             CountryListDialogHelper.show(this, mViewModel, mViewModel.phoneInputViewModel)
         }
